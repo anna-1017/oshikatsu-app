a26ef169d60522f3f243190be968b82573f7f67d
anna <alittlevegemite@gmail.com>
2025-06-16 16:41:43 +0900

    st_toppage view

---
+++ src/resources/views/st_top.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="jp">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ST FunFanトップページ</title>
+
+    <link rel="stylesheet" href="st.top.css">
+</head>
+
+<body>
+
+  <main class="toppage">
+    <div class="toppage__inner">
+      <header class="toppage__header">
+        <div class="header__inner">
+          <div class="header-logo">
+            <img src="SixTONES_logo.png" alt="logo" class="logo">
+          </div>
+        </div>
+      </header>
+      
+      <div class="contents">
+        <div class="left-contents">
+          <img src="SixTONES_Boyz_img.png" alt="top-img" class="top-img">
+        </div>
+
+        <div class="right-contents">
+          <p>💎Menu💎</p>
+            <a href="">・Profile</a><br>
+            <a href="">・Gallery</a><br>
+            <a href="">・Schedule</a><br>
+            <a href="">・Music</a><br>
+      
+        </div>
+
+      </div>
+
+
+    </div>
+
+
+
+  </main>
+</body>
+</html>
