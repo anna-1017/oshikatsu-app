6c51c2269683fa69779335d7350b6a25d008bf29
anna <alittlevegemite@gmail.com>
2025-06-23 16:48:15 +0900

    member profile

---
+++ src/resources/views/member_profile.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="jp">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Member Profile</title>
+
+    <link rel="stylesheet" href="member_profile.css">
+</head>
+
+<body>
+
+  <main class="member-profile">
+    <div class="member-profile__inner">
+      <header class="toppage__header">
+        <div class="header__inner">
+          <div class="header-logo">
+            <img src="SixTONES_logo.png" alt="logo" class="logo">
+          </div>
+        </div>
+      </header>
+
+    <!--6人の写真を3枚ずつ2段に並べて、クリックしたらここのページに飛ぶようにする -->
+      <div class="member-chart">
+        <a href="juri"><img src="Juri.png" alt="Juri">Juri Tanaka</a>
+        <a href="taiga"><img src="Taiga.png" alt="Taiga">Taiga Kyomoto</a>
+        <a href="yugo"><img src="Yugo.png" alt="Yugo">Yugo Kochi</a>
+        <a href="jesse"><img src="Jesse.png" alt="Jesse">Jesse</a>
+        <a href="hokuto"><img src="Hokuto.png" alt="Hokuto">Hokuto Matsumura</a>
+        <a href="shintaro"><img src="Shintaro.png" alt="Shintaro">Shintaro Morimoto</a>
+      </div>
+    </div>
+
+
+  </main>
+</body>
+</html>
