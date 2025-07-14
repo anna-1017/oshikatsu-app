45c36e1f321f3443b1c495f9099175b9cc0c9226
anna <alittlevegemite@gmail.com>
2025-07-14 16:55:00 +0900

    schedule7月6日まで

---
+++ src/resources/views/schedule.blade.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="jp">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Schedule</title>
+
+    <link rel="stylesheet" href="schedule.css">
+</head>
+
+<body>
+  <header class="toppage__header">
+    <div class="header__inner">
+      <div class="header-logo">
+        <img src="SixTONES_logo.png" alt="logo" class="logo">
+      </div>
+    </div>
+  </header>
+
+  <main class="schedule-page">
+    <h1 class="schedule">Schedule</h1>
+    <span class="month">2025.7</span>
+    <hr>
+    <div class="schedule-item">
+      <div class="leftside">
+        <h2 class="date">05</h2>
+        <h2 class="day">(SAT)</h2>
+      </div>
+      <div class="rightside">
+        <span class="tag">TV</span>
+        <span class="time">15:00-</span>
+        <div class="media-title">THE MUSIC DAY 2025 (日本テレビ系)</div>
+        <div class="notion">※生放送のため、予告なく内容が変更となる場合がございます。</div>
+
+        <div class="dotted-line"></div>
+
+        <span class="tag">RADIO</span>
+        <div class="time">23:30-</div>
+        <div class="media-title">SixTONESのオールナイトニッポンサタデースペシャル（ニッポン放送）</div>
+      </div>
+    </div>
+    
+    <div class="border"></div>
+    
+    <div class="schedule-item">
+      <div class="leftside">
+        <h2 class="date">06</h2>
+        <h2 class="day">(SUN)</h2>
+      </div>
+      <div class="rightside">
+        <span class="tag">TV</span>
+        <span class="time">11:45-</span>
+        <div class="media-title">スクール革命！ (日本テレビ系)</div>
+        
+        <div class="dotted-line"></div>
+
+        <span class="tag">TV</span>
+        <span class="time">19:00-</span>
+        <div class="media-title">ザ！鉄腕！DASH!! (日本テレビ系)</div>
+      
+
+        <div class="dotted-line"></div>
+
+        <span class="tag">TV</span>
+        <span class="time">21:00-</span>
+        <div class="media-title">Golden SixTONES (日本テレビ系)</div>
+      </div>
+    </div>
+    
+    <div class="border"></div>
+
+
+  
+
+    
+  </main>
+
+
+
+</body>
+</html>
