ca5b7f82f942dc5a19624c74fb6ee8b14a610b19
anna <alittlevegemite@gmail.com>
2025-07-22 17:12:38 +0900

    7月Schedule11日まで

--- src/resources/views/schedule.blade.php
+++ src/resources/views/schedule.blade.php
@@ -30,6 +30,7 @@
         <span class="tag">TV</span>
         <span class="time">15:00-</span>
         <div class="media-title">THE MUSIC DAY 2025 (日本テレビ系)</div>
+        <div class="who">SixTONES</div>
         <div class="notion">※生放送のため、予告なく内容が変更となる場合がございます。</div>
 
         <div class="dotted-line"></div>
@@ -37,6 +38,7 @@
         <span class="tag">RADIO</span>
         <div class="time">23:30-</div>
         <div class="media-title">SixTONESのオールナイトニッポンサタデースペシャル（ニッポン放送）</div>
+        <div class="who">SixTONES</div>
       </div>
     </div>
     
@@ -51,12 +53,14 @@
         <span class="tag">TV</span>
         <span class="time">11:45-</span>
         <div class="media-title">スクール革命！ (日本テレビ系)</div>
+        <div class="who">髙地優吾</div>
         
         <div class="dotted-line"></div>
 
         <span class="tag">TV</span>
         <span class="time">19:00-</span>
         <div class="media-title">ザ！鉄腕！DASH!! (日本テレビ系)</div>
+        <div class="who">森本慎太郎</div>
       
 
         <div class="dotted-line"></div>
@@ -64,12 +68,85 @@
         <span class="tag">TV</span>
         <span class="time">21:00-</span>
         <div class="media-title">Golden SixTONES (日本テレビ系)</div>
+        <div class="who">SixTONES</div>
       </div>
     </div>
     
     <div class="border"></div>
 
+    <div class="schedule-item">
+      <div class="leftside">
+        <h2 class="date">07</h2>
+        <h2 class="day">(MON)</h2>
+      </div>
+      <div class="rightside">
+        <span class="tag">MAGAZINE</span>
+        <div class="media-title">Duet 8月号</div>
+        <div class="who">森本慎太郎</div>
+      </div>
+    </div>
 
+    <div class="border"></div>
+
+    <div class="schedule-item">
+      <div class="leftside">
+        <h2 class="date">08</h2>
+        <h2 class="day">(TUE)</h2>
+      </div>
+      <div class="rightside">
+        <span class="tag">TV</span>
+        <span class="time">18:30-</span>
+        <div class="media-title">THE 神業チャレンジSP(TBS系)</div>
+        <div class="who">髙地優吾</div>
+      </div>
+    </div>
+    
+    
+    <div class="border"></div>
+
+    <div class="schedule-item">
+      <div class="leftside">
+        <h2 class="date">09</h2>
+        <h2 class="day">(WED)</h2>
+      </div>
+      <div class="rightside">
+        <span class="tag">MAGAZINE</span>
+        <div class="media-title">えんぶ8月号</div>
+        <div class="who">京本大我　※表紙</div>
+      </div>
+    </div>
+    
+    
+    <div class="border"></div>
+
+    <div class="schedule-item">
+      <div class="leftside">
+        <h2 class="date">10</h2>
+        <h2 class="day">(THU)</h2>
+      </div>
+      <div class="rightside">
+        <span class="tag">CONCERT</span>
+        <div class="media-title">TAIGA KYOMOTO LIVE TOUR 2025 BLUE OF LIBERTY（東京都）</div>
+        <div class="who">京本大我</div>
+      </div>
+    </div>
+    
+    
+    <div class="border"></div>
+    <div class="schedule-item">
+      <div class="leftside">
+        <h2 class="date">11</h2>
+        <h2 class="day">(FRI)</h2>
+      </div>
+      <div class="rightside">
+        <span class="tag">CONCERT</span>
+        <div class="media-title">TAIGA KYOMOTO LIVE TOUR 2025 BLUE OF LIBERTY（東京都）</div>
+        <div class="who">京本大我</div>
+      </div>
+    </div>
+    
+    
+    <div class="border"></div>
   
 
     
